8b838445fc06b00d97a2fe9dd9c9a2b18c21e9f5
Osca Gama <23.oscarr@gmail.com>
2023-03-12 14:02:13 -0500

    En poryecto 'app' ahora se guardan los graficos y se modifica app/main.py para que guarde los 2 graficos

--- app/charts.py
+++ app/charts.py
@@ -7,13 +7,15 @@ libreria matplotlib: https://matplotlib.org/
 def generate_bar_chart(labels, values): 
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.show()
+  plt.savefig('bar.png')
+  plt.close()
 
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels) 
   ax.axis('equal')
-  plt.show()
+  plt.savefig('pie.png')
+  plt.close()
 
 if __name__ == '__main__':
   labels = ['a','b','c']
--- app/main.py
+++ app/main.py
@@ -4,6 +4,13 @@ import charts
 
 def run():
   data = read_csv.read_csv('./app/data.csv')
+  data = list(filter(lambda x : x['Continent'] == 'South America', data))
+
+  country = list(map(lambda x: x['Country/Territory'], data))
+  percentage = list(map(lambda x: x['World Population Percentage'], data))
+  #print(percentage)
+  charts.generate_pie_chart(country, percentage)
+
   country = input('digita un pais: ')
   
   result = utils.population_by_county(data,country)
@@ -11,16 +18,8 @@ def run():
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
+    charts.generate_bar_chart(labels, values)    
 
-def run_pie():
-  data = read_csv.read_csv('./app/data.csv')
-  data = list(filter(lambda x : x['Continent'] == 'South America', data))
-  
-  country = list(map(lambda x: x['Country/Territory'], data))
-  percentage = list(map(lambda x: x['World Population Percentage'], data))
-  #print(percentage)
-  charts.generate_pie_chart(country, percentage)
   
 if __name__ == '__main__':
-  run_pie()
+  run()
