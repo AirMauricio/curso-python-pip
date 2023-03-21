117eb5f44e1eb3e1c0266591d2329e9c26b16de6
Osca Gama <23.oscarr@gmail.com>
2023-03-21 05:13:03 -0500

    Ajuste de oerden en la lineas en poryecto 'web-server'

--- web-server/store.py
+++ web-server/store.py
@@ -3,9 +3,12 @@ import requests # Libreria para traer información de una pagian Web
 def get_categories():
     r = requests.get('https://api.escuelajs.co/api/v1/categories')
     print(r.status_code)
-    print(r.text) # Lo trae en forma de String
-    cateogiries = r.json()
-
+    #print(r.text) # Lo trae en forma de String
+    
     print('*'*10)
+    
+    cateogiries = r.json()
+    print(cateogiries)
+
     for category in cateogiries:
         print(category['name'])
